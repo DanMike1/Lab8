950e1a4409a6f27add9ecb35a0e801501d01b406
DanMike1 <50786839+DanMike1@users.noreply.github.com>
2021-02-16 01:00:48 +0300

    Updated Exception

--- asset/js/06(exception).js
+++ asset/js/06(exception).js
@@ -1,3 +1,37 @@
+//ES6 Person Object 
+class Person_ES6 {
+    constructor(fName, lName) {
+        this.fName = fName;
+        this.lName = lName;
+    }
+    greeting() {
+        return `Hello,  ${this.fName} ${this.lName}`;
+    }
+}
+
+//1. Make Customer class to extend from Person_ES6
+class Customer_ES6 extends Person_ES6 {
+
+    constructor(fName, lName) {
+
+        //2. Call the super class [super(fName,lName)]
+        super(fName,lName)
+    
+    }
+}
+
+//3. Create Customer ["Jeor", "Mormont"]
+const customer_2 = new Customer_ES6()
+customer_2.fName = "Jeor"
+customer_2.lName = "Mormont"
+
+
+//4. Display the greeting on Jeor Customer Object [remove the string when you have the object]
+es6_proto.innerHTML = customer_2.greeting();
+
+END
+
+
 //Exception Handling using try catch 
 const fitsNo = document.querySelector("#fn");
 const secondNo = document.querySelector("#sn");
@@ -18,16 +52,18 @@ function divideNo() {
     }
 
     // 1. Define a try block 
+    try{    
+        const result = performDivision(no1, no2);
+
+        res.innerHTML = result;
+    }
 
     // 2. put this two lines of code inside the try block 
-
-    const result = performDivision(no1, no2);
-
-    res.innerHTML = result;
-
+    
+    
 
     //4.Catch the exception and display error message [remove the string when you have the error variable]
-    res.innerHTML = "error";
+   catch(error) {res.innerHTML = "Can Not Divide by zero"};
 
 }
 
