9013b152794b2401c53362ce248ed6caf9124517
DanMike1 <50786839+DanMike1@users.noreply.github.com>
2021-02-16 00:31:34 +0300

    OOP3 Done

--- asset/js/05(OOP3).js
+++ asset/js/05(OOP3).js
@@ -1,16 +1,24 @@
 // 1. Create a Person_ES6 Class 
-
-
-
-//2. Create the constructor that initialize firstName and lastName
-
+class Person_ES6
+{
+    //2. Create the constructor that initialize firstName and lastName
+constructor (firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+}
+    greeting() {
+        return `Hi ${this.firstName} ${this.lastName}`;
+    }
+}
 
 //3. Define greeting() method that displays Full Name
 
 
 //4. Create  Object using ES6 class by passing firstName,lastName ["Bran Stark"]
-
+const Bran = new Person_ES6()
+Bran.firstName ="Bran"
+Bran.lastName = "Stark"
 
 
 //5. Display the greeting on Bran Object [remove the string when you have the object]
-Obj_C3.innerHTML = "Bran.greeting()";
+Obj_C3.innerHTML = Bran.greeting();
